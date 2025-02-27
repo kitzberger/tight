41f312f8442dfbaad59b30fd1e792003d349eb0f
Philipp Kitzberger <coding@kitze.net>
2025-02-27 12:01:39 +0100

    Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Tight Backend',
+    'description' => 'Tweaks the backend CSS of TYPO3 **13.4** to reduce the vast space set between the UI elements.',
+    'category' => 'system',
+    'state' => 'stable',
+    'author' => 'Philipp Kitzberger',
+    'author_email' => 'typo3@kitze.net',
+    'author_company' => '',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+            'form' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
